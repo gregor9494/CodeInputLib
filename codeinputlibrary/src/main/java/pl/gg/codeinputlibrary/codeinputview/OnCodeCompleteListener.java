ec19431129976723462819db6580b371169085bc
Gregor94 <g.gaworowski@graphicbox.pl>
2018-02-23 09:53:59 +0100

    init

---
+++ codeinputlibrary/src/main/java/pl/gg/codeinputlibrary/codeinputview/OnCodeCompleteListener.java
@@ -0,0 +1,8 @@
+package pl.gg.codeinputlibrary.codeinputview;
+
+/**
+ * Copyright (c) Tuenti Technologies. All rights reserved.
+ */
+public interface OnCodeCompleteListener {
+	void onCompleted(String code);
+}
